f888faeb8be52bef0a925ae80c281286d7c05d5f
SungminPark <rnfxl92@naver.com>
2021-04-09 16:39:58 +0900

    [feat] location test

--- project04-TodoTDD/project04-TodoTDD/Model/Location.swift
+++ project04-TodoTDD/project04-TodoTDD/Model/Location.swift
@@ -12,4 +12,16 @@ struct Location {
     let name: String
     let coordinate: CLLocationCoordinate2D?
     
+    init(name: String, coordinate: CLLocationCoordinate2D? = nil) {
+        self.name = name
+        self.coordinate = coordinate
+    }
+}
+
+extension Location: Equatable {
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        return lhs.name == rhs.name
+            && lhs.coordinate?.latitude == rhs.coordinate?.latitude
+            && lhs.coordinate?.longitude == rhs.coordinate?.longitude
+    }
 }
--- project04-TodoTDD/project04-TodoTDDTests/Models/LocationTests.swift
+++ project04-TodoTDD/project04-TodoTDDTests/Models/LocationTests.swift
@@ -10,17 +10,25 @@ import CoreLocation
 @testable import project04_TodoTDD
 
 class LocationTests: XCTestCase {
-
+    let locationName = "LocationName"
+   
     func test_init_Name() {
         
+        let location = Location(name: locationName)
+        
+        XCTAssertEqual(locationName, location.name)
     }
     
     func test_init_Coordinate() {
+        let latitude = 5.0
+        let longitude = 10.0
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let location = Location(name: locationName, coordinate: coordinate)
+        
+        XCTAssertEqual(location.coordinate?.latitude, latitude)
+        XCTAssertEqual(location.coordinate?.longitude, longitude)
         
     }
     
-    func test_Serialize_And_Deserialize() {
-        
-    }
 
 }
