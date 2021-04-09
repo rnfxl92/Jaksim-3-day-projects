378546512e79fd2cadb6065cb4b1eb8683bf3ea3
SungminPark <rnfxl92@naver.com>
2021-04-09 21:36:08 +0900

    [feat] ToDoItem Test

--- project04-TodoTDD/project04-TodoTDD/Model/ToDoItem.swift
+++ project04-TodoTDD/project04-TodoTDD/Model/ToDoItem.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 struct ToDoItem {
+    let title: String
+    let description: String?
+    let timeStamp: Date?
+    let location: Location?
     
+    init(title: String, description: String? = nil, timeStamp: Date? = nil, location: Location? = nil) {
+        self.title = title
+        self.description = description
+        self.timeStamp = timeStamp
+        self.location = location
+    }
+    
+}
+
+extension ToDoItem: Equatable {
+    static func ==(lhs: ToDoItem, rhs: ToDoItem) -> Bool {
+        return lhs.title == rhs.title && lhs.location?.name == rhs.location?.name && lhs.timeStamp == rhs.timeStamp
+    }
 }
--- project04-TodoTDD/project04-TodoTDDTests/Models/ToDoItemTests.swift
+++ project04-TodoTDD/project04-TodoTDDTests/Models/ToDoItemTests.swift
@@ -8,24 +8,35 @@
 import XCTest
 
 class ToDoItemTests: XCTestCase {
-    let timestamp = 0.0
-    let locationName = "Location"
+    let titleName = "TitleName"
     
     func test_init_Title() {
-        
+        let toDoItem = ToDoItem(title: titleName)
+        XCTAssertEqual(toDoItem.title, titleName)
     }
 
     func test_init_Description() {
+        let itemDescription = "Description"
+        let toDoItem = ToDoItem(title: titleName, description: itemDescription)
+        
+        XCTAssertEqual(toDoItem.description, itemDescription)
         
     }
     
     func test_init_TimeStamp() {
+        let timeStamp = Date()
+        let toDoItem = ToDoItem(title: titleName, timeStamp: timeStamp)
         
+        XCTAssertEqual(toDoItem.timeStamp, timeStamp)
     }
     
     func test_init_Location() {
+        let locationName = "Location"
+        let location = Location(name: locationName)
+        let toDoItem = ToDoItem(title: titleName, location: location)
+        
+        XCTAssertEqual(toDoItem.location, location)
         
     }
-    
-    
+        
 }
