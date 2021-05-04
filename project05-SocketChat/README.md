6a0c6df9152ffd4a3b891b12617f596092a040ef
SungminPark <rnfxl92@naver.com>
2021-05-04 16:15:34 +0900

    [feat]project05 시작

---
+++ project05-SocketChat/README.md
@@ -0,0 +1,3 @@
+# Project05 - SocketChat
+
+https://github.com/appcoda/SocketIOChat 의 소켓 채팅을 구현
---
+++ project05-SocketChat/SocketChat/UsersViewController.swift
@@ -0,0 +1,115 @@
+//
+//  UsersViewController.swift
+//  SocketChat
+//
+//  Created by Gabriel Theodoropoulos on 1/31/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tblUserList: UITableView!
+    
+    
+    var users = [[String: AnyObject]]()
+    
+    var nickname: String!
+    
+    var configurationOK = false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if !configurationOK {
+            configureNavigationBar()
+            configureTableView()
+            configurationOK = true
+        }
+        
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            if identifier == "idSegueJoinChat" {
+                let chatViewController = segue.destinationViewController as! ChatViewController
+                chatViewController.nickname = nickname
+            }
+        }
+    }
+
+    
+    // MARK: IBAction Methods
+    
+    @IBAction func exitChat(sender: AnyObject) {
+        
+    }
+
+    
+    
+    // MARK: Custom Methods
+    
+    func configureNavigationBar() {
+        navigationItem.title = "SocketChat"
+    }
+    
+    
+    func configureTableView() {
+        tblUserList.delegate = self
+        tblUserList.dataSource = self
+        tblUserList.registerNib(UINib(nibName: "UserCell", bundle: nil), forCellReuseIdentifier: "idCellUser")
+        tblUserList.hidden = true
+        tblUserList.tableFooterView = UIView(frame: CGRectZero)
+    }
+    
+    
+    // MARK: UITableView Delegate and Datasource methods
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("idCellUser", forIndexPath: indexPath) as! UserCell
+        
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 44.0
+    }
+    
+}
