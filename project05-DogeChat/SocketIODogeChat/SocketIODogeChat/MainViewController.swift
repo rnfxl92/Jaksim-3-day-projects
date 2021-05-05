d1f65449d3f565d5376f71481f7beb2bbccdbd14
SungminPark <rnfxl92@naver.com>
2021-05-06 00:11:59 +0900

    [feat] MainViewController

---
+++ project05-DogeChat/SocketIODogeChat/SocketIODogeChat/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  SocketIODogeChat
+//
+//  Created by 박성민 on 2021/05/05.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
