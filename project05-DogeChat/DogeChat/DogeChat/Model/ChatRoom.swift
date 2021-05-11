64173c5da294320fdabef72b637c0f321e2a5abc
SungminPark <rnfxl92@naver.com>
2021-05-11 20:18:07 +0900

    [feat, docs] 내가 구현한걸로 교체

--- project05-DogeChat/DogeChat/DogeChat/Model/ChatRoom.swift
+++ project05-DogeChat/DogeChat/DogeChat/Model/ChatRoom.swift
@@ -1,41 +1,20 @@
-/// Copyright (c) 2021 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+//
+//  ChatRoom.swift
+//  SocketIODogeChat
+//
+//  Created by 박성민 on 2021/05/07.
+//
 
-import Foundation
+import UIKit
 
 protocol ChatRoomDelegate: AnyObject {
   func received(message: Message)
 }
 
-
 class ChatRoom: NSObject {
   var inputStream: InputStream!
   var outputStream: OutputStream!
+  
   weak var delegate: ChatRoomDelegate?
   var username = ""
   
@@ -65,15 +44,13 @@ class ChatRoom: NSObject {
   
   func joinChat(username: String) {
     let data = "iam:\(username)".data(using: .utf8)!
-    
     self.username = username
     
-    _ = data.withUnsafeBytes {
+    data.withUnsafeBytes {
       guard let pointer = $0.baseAddress?.assumingMemoryBound(to: UInt8.self) else {
         print("Error joining chat")
         return
       }
-      
       outputStream.write(pointer, maxLength: data.count)
     }
   }
@@ -81,7 +58,7 @@ class ChatRoom: NSObject {
   func send(message: String) {
     let data = "msg:\(message)".data(using: .utf8)!
     
-    _ = data.withUnsafeBytes {
+    data.withUnsafeBytes {
       guard let pointer = $0.baseAddress?.assumingMemoryBound(to: UInt8.self) else {
         print("Error joining chat")
         return
@@ -94,7 +71,7 @@ class ChatRoom: NSObject {
     inputStream.close()
     outputStream.close()
   }
-  
+
 }
 
 extension ChatRoom: StreamDelegate {
@@ -117,7 +94,6 @@ extension ChatRoom: StreamDelegate {
   
   private func readAvailableBytes(stream: InputStream) {
     let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: maxReadLength)
-    
     while stream.hasBytesAvailable {
       let numberOfBytesRead = inputStream.read(buffer, maxLength: maxReadLength)
       
@@ -134,20 +110,20 @@ extension ChatRoom: StreamDelegate {
   
   private func processedMessageString(buffer: UnsafeMutablePointer<UInt8>,
                                       length: Int) -> Message? {
-    guard let stringArray = String(
-            bytesNoCopy: buffer,
-            length: length,
-            encoding: .utf8,
-            freeWhenDone: true)?.components(separatedBy: ":"),
-          let name = stringArray.first,
-          let message = stringArray.last
-          else {
-      return nil
-    }
-      
-    let messageSender: MessageSender = (name == self.username) ? .ourself : .someoneElse
     
+    guard
+      let stringArray = String(
+        bytesNoCopy: buffer,
+        length: length,
+        encoding: .utf8,
+        freeWhenDone: true)?.components(separatedBy: ":"),
+      let name = stringArray.first,
+      let message = stringArray.last
+      else {
+        return nil
+    }
+
+    let messageSender: MessageSender = (name == self.username) ? .ourself : .someoneElse
     return Message(message: message, messageSender: messageSender, username: name)
   }
-  
 }
--- project05-DogeChat/README.md
+++ project05-DogeChat/README.md
@@ -1,3 +1,7 @@
 # Project05 - DogeChat
 
 https://www.raywenderlich.com/3437391-real-time-communication-with-streams-tutorial-for-ios를 구현
+
+## Screenshots
+![join](./join.png)
+![chat](./chat.png)
