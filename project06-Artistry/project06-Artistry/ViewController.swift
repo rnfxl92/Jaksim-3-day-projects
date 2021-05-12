a353520c193f58cc2fb33c059d555356c91e3cee
SungminPark <rnfxl92@naver.com>
2021-05-13 02:09:23 +0900

    [feat] Start project06

---
+++ project06-Artistry/project06-Artistry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project06-Artistry
+//
+//  Created by 박성민 on 2021/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
