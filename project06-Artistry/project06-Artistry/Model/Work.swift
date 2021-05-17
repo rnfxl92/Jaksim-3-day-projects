25aa6503b1bba3407e3e2d17d5cbf13ae4e58dc2
SungminPark <rnfxl92@naver.com>
2021-05-17 19:31:17 +0900

    [feat] work model

---
+++ project06-Artistry/project06-Artistry/Model/Work.swift
@@ -0,0 +1,16 @@
+//
+//  Work.swift
+//  project06-Artistry
+//
+//  Created by 박성민 on 2021/05/17.
+//
+
+import UIKit
+
+struct Work {
+    let title: String
+    let image: UIImage
+    let info: String
+    var isExpanded: Bool
+}
+
