b7cecd78ec84c5ddfca1e27f3d07b1660f46ddba
SungminPark <rnfxl92@naver.com>
2021-05-17 23:14:05 +0900

    [refactor] UITableViewController -> UIViewController

---
+++ project06-Artistry/project06-Artistry/ViewController/ArtistListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ArtistListViewController.swift
+//  project06-Artistry
+//
+//  Created by 박성민 on 2021/05/17.
+//
+
+import UIKit
+
+class ArtistListViewController: UIViewController {
+    let artists = Artist.artistsFromBundle()
+    
+    override func viewDidLoad() {
+        
+    }
+}
