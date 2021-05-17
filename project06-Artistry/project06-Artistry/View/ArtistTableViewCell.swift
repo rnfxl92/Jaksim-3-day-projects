118bd9255d793cfce1d70488d073e310f5f54b69
SungminPark <rnfxl92@naver.com>
2021-05-18 00:08:54 +0900

    [feat] ArtistListViewContoller

---
+++ project06-Artistry/project06-Artistry/View/ArtistTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArtistTableViewCell.swift
+//  project06-Artistry
+//
+//  Created by 박성민 on 2021/05/18.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- project06-Artistry/project06-Artistry/ViewController/ArtistListViewController.swift
+++ project06-Artistry/project06-Artistry/ViewController/ArtistListViewController.swift
@@ -10,7 +10,36 @@ import UIKit
 class ArtistListViewController: UIViewController {
     let artists = Artist.artistsFromBundle()
     
+    @IBOutlet weak var artistTableView: UITableView!
+    
     override func viewDidLoad() {
+        artistTableView.dataSource = self
+        artistTableView.delegate = self
+        artistTableView.rowHeight = UITableView.automaticDimension
+        artistTableView.estimatedRowHeight = 150
         
+        NotificationCenter.default.addObserver(forName: UIContentSizeCategory.didChangeNotification, object: .none, queue: OperationQueue.main) { [weak self] _ in
+            self?.artistTableView.reloadData()
+            
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //if let destination = segue.description as?
+    }
+    
+}
+
+extension ArtistListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return artists.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ArtistTableViewCell
+      
+      let artist = artists[indexPath.row]
+      
+      return cell
     }
 }
